4213cc3aa46cbcc7562fcc197fc585b33cff76e1
astra <26215963+3tternp@users.noreply.github.com>
2025-05-30 08:36:08 +0545

    Create views.py

---
+++ backend/core/views.py
@@ -0,0 +1,9 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAdmin
+
+class AdminDashboard(APIView):
+    permission_classes = [IsAuthenticated, IsAdmin]
+    def get(self, request):
+        return Response({"message": "Welcome Admin"})
