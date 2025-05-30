95c91374af830b8a1384749fd57c3e4e7a401e66
astra <26215963+3tternp@users.noreply.github.com>
2025-05-30 21:07:55 +0545

    Update serializers.py

--- backend/controls/serializers.py
+++ backend/controls/serializers.py
@@ -1,1 +1,13 @@
+from rest_framework import serializers
+from .models import Framework, Control
+
+class FrameworkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Framework
+        fields = '__all__'
+
+class ControlSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Control
+        fields = '__all__'
 
