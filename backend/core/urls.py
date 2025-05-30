594ff7bbb625fbb85d7f4a2b676c5ce20ef584c3
astra <26215963+3tternp@users.noreply.github.com>
2025-05-30 08:48:33 +0545

    Update urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,14 @@
+
 from django.urls import path
 from .views import AdminDashboard
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import RiskViewSet, AdminDashboard
+
+router = DefaultRouter()
+router.register(r'risks', RiskViewSet)
 
 urlpatterns = [
     path('admin/dashboard/', AdminDashboard.as_view()),
+    path('', include(router.urls)),
 ]
