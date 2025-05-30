5a091a64a699ba28c4b7b18b4a27e7ea109ef65f
astra <26215963+3tternp@users.noreply.github.com>
2025-05-30 21:09:30 +0545

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/core/', include('core.urls')),
+    path('api/controls/', include('controls.urls')),
+
 ]
