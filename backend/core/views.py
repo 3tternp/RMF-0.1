b5eeb9520515382cbdd8f1cc169714593e77b4bb
astra <26215963+3tternp@users.noreply.github.com>
2025-05-30 08:46:33 +0545

    Update views.py

--- backend/core/views.py
+++ backend/core/views.py
@@ -2,8 +2,18 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAdmin
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Risk
+from .serializers import RiskSerializer
+from .permissions import ReadOnlyOrAdminManager
 
 class AdminDashboard(APIView):
     permission_classes = [IsAuthenticated, IsAdmin]
     def get(self, request):
         return Response({"message": "Welcome Admin"})
+
+class RiskViewSet(viewsets.ModelViewSet):
+    queryset = Risk.objects.all()
+    serializer_class = RiskSerializer
+    permission_classes = [IsAuthenticated, ReadOnlyOrAdminManager]
