4a6ead6784c6c5b8c4e4bef6e2922badc5b60071
astra <26215963+3tternp@users.noreply.github.com>
2025-05-30 20:58:22 +0545

    Update serializers.py

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Risk
+from .models import User, Risk
 
 class RiskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Risk
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'role']
