ab83115e0a645b2757b3d0aa281f61299476a7a5
astra <26215963+3tternp@users.noreply.github.com>
2025-05-30 21:08:38 +0545

    Update urls.py

--- backend/controls/urls.py
+++ backend/controls/urls.py
@@ -1,1 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from .views import FrameworkViewSet, ControlViewSet
+
+router = DefaultRouter()
+router.register('frameworks', FrameworkViewSet, basename='frameworks')
+router.register('controls', ControlViewSet, basename='controls')
+
+urlpatterns = router.urls
 
