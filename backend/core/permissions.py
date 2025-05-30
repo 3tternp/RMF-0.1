6b886c8232aab2c3b1c60b1944c5f887c9da5b11
astra <26215963+3tternp@users.noreply.github.com>
2025-05-30 08:42:45 +0545

    Update permissions.py

--- backend/core/permissions.py
+++ backend/core/permissions.py
@@ -1,9 +1,11 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-class IsAdmin(BasePermission):
+class IsAdminOrManager(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'admin'
+        return request.user.is_authenticated and request.user.role in ['admin', 'manager']
 
-class IsManager(BasePermission):
+class ReadOnlyOrAdminManager(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'manager'
+        if request.method in SAFE_METHODS:
+            return request.user.is_authenticated
+        return request.user.is_authenticated and request.user.role in ['admin', 'manager']
